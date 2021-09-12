a29b10751a40c0aeccdac74c4dadeba118d711a8
Md. Zahidul Islam <vdjsovaj@gmail.com>
2021-09-12 19:24:32 +0600

    update slngleMovie xml

--- app/src/main/java/dev/jahidhasanco/fliq/ui/adapter/PopularMovieAdapter.kt
+++ app/src/main/java/dev/jahidhasanco/fliq/ui/adapter/PopularMovieAdapter.kt
@@ -40,37 +40,6 @@ class PopularMovieAdapter(val ctx: Context, val movies: List<Result>) :
             .load(Util.posterUrlMake(movie.posterPath))
             .into(viewHolder.poster)
 
-
-        viewHolder.movieTitle.text = movie.title
-        viewHolder.releaseDate.text = movie.releaseDate
-        viewHolder.genre1.text = Constants.getGenre(movie.genreIds[0])
-
-        if (movie.genreIds.size > 1) {
-            viewHolder.genre2.text = Constants.getGenre(movie.genreIds[1])
-            viewHolder.genre2Layout.visibility = View.VISIBLE
-        } else {
-            viewHolder.genre2Layout.visibility = View.INVISIBLE
-        }
-
-
-//        Glide.with(ctx)
-//            .load(Util.posterUrlMake(movie.posterPath))
-//            .placeholder(R.drawable.poster_bg)
-//            .into(object : CustomTarget<Drawable>(1080, 1080) {
-//                override fun onResourceReady(
-//                    resource: Drawable,
-//                    transition: Transition<in Drawable>?
-//                ) {
-//                    viewHolder!!.poster.setImageDrawable(resource)
-//                }
-//
-//                override fun onLoadCleared(placeholder: Drawable?) {
-//
-//                }
-//
-//            })
-
-
         viewHolder.itemView.setOnClickListener {
             val intent = Intent(ctx,MovieDetailsActivity::class.java)
             val movieId:String = movie.id.toString()
@@ -87,12 +56,6 @@ class PopularMovieAdapter(val ctx: Context, val movies: List<Result>) :
 
     class MyViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         val poster = itemView.findViewById<ImageView>(R.id.imageView_single_movie)
-        val movieTitle = itemView.findViewById<TextView>(R.id.title_single_movie)
-        val releaseDate = itemView.findViewById<TextView>(R.id.date_single_movie)
-        val genre1 = itemView.findViewById<TextView>(R.id.genre1_movie)
-        val genre2 = itemView.findViewById<TextView>(R.id.genre2_movie)
-        val genre2Layout = itemView.findViewById<LinearLayout>(R.id.genre2Layout_movie)
-
 
     }
 }
