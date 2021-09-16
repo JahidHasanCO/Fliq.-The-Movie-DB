fc9f7233fef4ad41de6f15db55ddb0a3149dc853
Md. Zahidul Islam <vdjsovaj@gmail.com>
2021-09-16 11:48:13 +0600

    colleapase tool bar

--- app/src/main/java/dev/jahidhasanco/fliq/ui/activity/SeeAllMovieActivity.kt
+++ app/src/main/java/dev/jahidhasanco/fliq/ui/activity/SeeAllMovieActivity.kt
@@ -2,6 +2,7 @@ package dev.jahidhasanco.fliq.ui.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
 import dev.jahidhasanco.fliq.R
 
 class SeeAllMovieActivity : AppCompatActivity() {
@@ -11,6 +12,11 @@ class SeeAllMovieActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_see_all_movie)
+
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
+            WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS
+        );
 
         comeFrom = intent.getStringExtra("ComeFrom").toString()
 
