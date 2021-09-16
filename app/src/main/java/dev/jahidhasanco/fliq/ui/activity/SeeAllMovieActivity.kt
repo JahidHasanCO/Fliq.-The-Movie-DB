6da98fb5d215ca3f8d0362125022f4c8ea82762d
Md. Zahidul Islam <vdjsovaj@gmail.com>
2021-09-16 13:10:32 +0600

    popular movies

--- app/src/main/java/dev/jahidhasanco/fliq/ui/activity/SeeAllMovieActivity.kt
+++ app/src/main/java/dev/jahidhasanco/fliq/ui/activity/SeeAllMovieActivity.kt
@@ -2,11 +2,23 @@ package dev.jahidhasanco.fliq.ui.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.view.WindowManager
 import android.widget.TextView
 import androidx.appcompat.widget.Toolbar
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.appbar.CollapsingToolbarLayout
+import com.smarteist.autoimageslider.IndicatorView.animation.type.IndicatorAnimationType
+import com.smarteist.autoimageslider.SliderAnimations
 import dev.jahidhasanco.fliq.R
+import dev.jahidhasanco.fliq.data.viewModel.MovieViewModel
+import dev.jahidhasanco.fliq.ui.adapter.PopularMovieAdapter
+import dev.jahidhasanco.fliq.ui.adapter.SeeAllMovieAdapter
+import dev.jahidhasanco.fliq.ui.adapter.silder.MovieSliderAdapter
 import org.w3c.dom.Text
 
 class SeeAllMovieActivity : AppCompatActivity() {
@@ -15,6 +27,10 @@ class SeeAllMovieActivity : AppCompatActivity() {
 
     lateinit var toolbar: Toolbar
     lateinit var collapsingToolbar_seeAllMovies: CollapsingToolbarLayout
+    lateinit var recyclerView_seeAllMovies: RecyclerView
+
+    lateinit var seeAllMovieAdapter: SeeAllMovieAdapter
+    lateinit var movieViewModel: MovieViewModel
 
     lateinit var textTitle_seeAllMovies: TextView
 
@@ -30,15 +46,17 @@ class SeeAllMovieActivity : AppCompatActivity() {
         toolbar = findViewById(R.id.toolbar_seeAllMovies)
         collapsingToolbar_seeAllMovies = findViewById(R.id.collapsingToolbar_seeAllMovies)
         textTitle_seeAllMovies = findViewById(R.id.textTitle_seeAllMovies)
+        recyclerView_seeAllMovies = findViewById(R.id.recyclerView_seeAllMovies)
         setSupportActionBar(toolbar)
 
-
+        movieViewModel = ViewModelProvider(this).get(MovieViewModel::class.java)
 
 
         when(comeFrom){
             "PopularMovies" -> {
                 collapsingToolbar_seeAllMovies.title = "Popular Movies"
                 textTitle_seeAllMovies.text = "Popular Movies"
+                observeViewModel()
             }
             else -> {
                 collapsingToolbar_seeAllMovies.title = ""
@@ -49,4 +67,63 @@ class SeeAllMovieActivity : AppCompatActivity() {
 
 
     }
+
+    private fun observeViewModel() {
+
+
+        if(comeFrom == "PopularMovies"){
+            getPopularMovies()
+        }
+
+
+        movieViewModel.movieLoadError.observe(this, Observer { isError ->
+
+
+//            if(isError == "" || isError == null){
+//                noInternet_Layout_movieFragment.visibility =  View.GONE
+//                image_slider_movieFragment.visibility = View.VISIBLE
+//                popular_MovieLayout_movieFrag.visibility = View.VISIBLE
+//                topRated_MovieLayout_movieFrag.visibility = View.VISIBLE
+//            }else{
+//                noInternet_Layout_movieFragment.visibility =  View.VISIBLE
+//                image_slider_movieFragment.visibility = View.INVISIBLE
+//                popular_MovieLayout_movieFrag.visibility = View.INVISIBLE
+//                topRated_MovieLayout_movieFrag.visibility = View.INVISIBLE
+//            }
+
+        })
+        movieViewModel.loading.observe(this, Observer { isLoading ->
+            isLoading?.let {
+
+
+//                animationView_movieFragment.visibility = if(it) View.VISIBLE else View.GONE
+//                if(it) {
+//                    image_slider_movieFragment.visibility = View.GONE
+//                    popular_MovieLayout_movieFrag.visibility = View.GONE
+//                    topRated_MovieLayout_movieFrag.visibility = View.GONE
+//                }
+
+            }
+        })
+    }
+
+    private fun getPopularMovies() {
+        movieViewModel.PopularMovies.observe(this, Observer {movies ->
+            movies?.let {
+
+                seeAllMovieAdapter = SeeAllMovieAdapter(this,it)
+                recyclerView_seeAllMovies.apply {
+                    adapter = seeAllMovieAdapter
+                    layoutManager = GridLayoutManager(this@SeeAllMovieActivity,3)
+                    setHasFixedSize(false)
+
+                }
+
+
+            }
+
+        })
+    }
+
+
 }
